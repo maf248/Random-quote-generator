2be3af019102acf7aaeea66ab29cf89097593448
maf248 <maf_248@hotmail.com>
2021-07-20 19:34:55 -0300

    Implementé el llamado a JSON Bin para obtener las citas, mediante fetch, siendo parseado y manejada la informacion mediante estados de react. Además converti el componente QuoteBox a componente de clase, y por ultimo implementé que seleccione un frase y su correspondiente autor aleatoriamente al obtener los resultados de JSON Bin, además hice que el boton New Quote muestre una nueva cita y autor, actualizando el estado de react.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ import QuoteBox from './Components/Quote-box';
 function App() {
   return (
 <div id="wrapper" className="App">
+    <div className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
+    <h1>Random Ecology Quote Generator</h1>
+    </div>
+    
     <QuoteBox quote="Connecting..." author="Please wait." />
     <div className="footer">by <a href="https://codepen.io/maf248/">MaF!</a></div>
 </div>
--- src/Components/Quote-box.js
+++ src/Components/Quote-box.js
@@ -1,34 +1,83 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuoteLeft, faCopy } from '@fortawesome/free-solid-svg-icons';
 import { faWhatsapp, faTwitter, faTumblr } from '@fortawesome/free-brands-svg-icons';
 
+class QuoteBox extends Component {
 
+	constructor () {
+		super();
+		this.state = {
+			quotes: [],
+            quote: "Connecting...",
+            author: "Please wait."
+		};
+        this.newQuoteHandler = this.newQuoteHandler.bind(this);
+	}
+    
+    newQuoteHandler(event) {
+        var positionRandom = Math.floor(Math.random()*this.state.quotes.length);
 
-function QuoteBox(props) {
-    return (
-        <div id="quote-box">
-        <div className="quote-text">
-            <FontAwesomeIcon icon={faQuoteLeft} /><span id="text">{props.quote}</span>
+        this.setState({
+        author: this.state.quotes[positionRandom].author,
+        quote: this.state.quotes[positionRandom].quote
+      });
+      
+      }
+
+    componentDidMount () {
+
+		fetch('https://api.jsonbin.io/b/60ec8df4c68da87103080bf6')
+			.then(res => res.json())
+			.then(response => {
+                console.log(response);
+                var positionRandom = Math.floor(Math.random()*response.quotes.length);
+				this.setState({
+					quotes: response.quotes,
+                    author: response.quotes[positionRandom].author,
+                    quote: response.quotes[positionRandom].quote
+
+				})
+			})
+			.catch((e) => {
+				console.log(e);
+			})
+
+	}
+
+	// componentDidUpdate (prevProps, prevState) {
+    //     if (prevState.quote !== this.state.quote) {
+            
+    //     }
+	// }
+    
+    render() {
+        return (
+            <div id="quote-box">
+            <div className="quote-text">
+                <FontAwesomeIcon icon={faQuoteLeft} />
+                <br/>
+                <span id="text">{this.state.quote}</span>
+            </div>
+            <div className="quote-author">~ <span id="author">{this.state.author}</span></div>
+            <div className="buttons">
+                <button className="btn btn-secondary button" id="copy-quote" title="Copy to clipboard!">
+                    <FontAwesomeIcon icon={faCopy} />
+                </button>
+                <button className="btn btn-secondary button" id="whatsapp-quote" title="Whatsapp this quote!" target="_top">
+                    <FontAwesomeIcon icon={faWhatsapp} />
+                </button>
+                <button className="btn btn-secondary button" id="tweet-quote" title="Tweet this quote!" target="_top">
+                    <FontAwesomeIcon icon={faTwitter} />
+                </button>
+                <button className="button" id="tumblr-quote" title="Post this quote on tumblr!" target="_blank">
+                    <FontAwesomeIcon icon={faTumblr} />
+                </button>
+                <button className="button btn btn-secondary" id="new-quote" onClick={this.newQuoteHandler.bind(this)}>New quote</button>
+            </div>
         </div>
-        <div className="quote-author">~ <span id="author">{props.author}</span></div>
-        <div className="buttons">
-            <button className="btn btn-secondary button" id="copy-quote" title="Copy to clipboard!">
-                <FontAwesomeIcon icon={faCopy} />
-            </button>
-            <button className="btn btn-secondary button" id="whatsapp-quote" title="Whatsapp this quote!" target="_top">
-                <FontAwesomeIcon icon={faWhatsapp} />
-            </button>
-            <button className="btn btn-secondary button" id="tweet-quote" title="Tweet this quote!" target="_top">
-                <FontAwesomeIcon icon={faTwitter} />
-            </button>
-            <button className="button" id="tumblr-quote" title="Post this quote on tumblr!" target="_blank">
-                <FontAwesomeIcon icon={faTumblr} />
-            </button>
-            <button className="button btn btn-secondary" id="new-quote">New quote</button>
-        </div>
-    </div>
-    );
+        );
+    }
 }
 
 export default QuoteBox;
